a640a0ca2d467b3eec060871790f2cebd70197ad
pablo <pablo.cda@gmail.com>
2020-11-13 18:35:35 -0300

    agrego parametro para modificar el tiempo que debe tener un archivo para ser borrado

--- delete_pending.py
+++ delete_pending.py
@@ -3,13 +3,19 @@
 import os
 import time
 import subprocess
+import argparse
 
 now = time.time()
 
+parser = argparse.ArgumentParser(description='Remove files from /var/spool/sympa/subscribe/ older than 24 hs, by default')
+parser.add_argument('--time-lapse',"-t", help='Number of hours required to delete a file', default="24")
+
 folder = '/var/spool/sympa/subscribe/'
+
+time_lapse = int(parser.parse_args().time_lapse)
 
 files = [os.path.join(folder, filename) for filename in os.listdir(folder)]
 for filename in files:
     hs_lapse = (now - os.stat(filename).st_mtime) / 3600
-    if (filename.endswith('add')) and ( hs_lapse > 24):
+    if (filename.endswith('add')) and ( hs_lapse > time_lapse):
         os.remove(filename)
